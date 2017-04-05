1c891922bb3fe7e556cbb26944486e4e533e09c3
Joshua Karstendick <jkarstendick@rjmetrics.com>
2017-04-05 17:02:00 -0400

    I used xpath today

--- tap_adwords/__init__.py
+++ tap_adwords/__init__.py
@@ -4,6 +4,13 @@ import datetime
 import os
 import sys
 
+import xml.etree.ElementTree as ET
+from suds.client import Client
+import time
+import googleads
+from googleads import adwords
+from googleads import oauth2
+
 import requests
 import singer
 
@@ -12,7 +19,8 @@ from singer import utils
 
 LOGGER = singer.get_logger()
 SESSION = requests.Session()
-
+PAGE_SIZE = 100
+VERSION = 'v201702'
 CAMPAIGNS_FIELD_LIST = ["AdvertisingChannelSubType"
                                       "AdvertisingChannelType"
                                       "Amount"
@@ -115,46 +123,121 @@ def get_url(endpoint):
     return BASE_URL.format(CONFIG['account_name']) + endpoint
 
 @utils.ratelimit(100, 15) # TODO
-def request(url, params=None, body=None):
-    params = params or {}
-    headers = {"Accept": "application/json",
-               "Authorization": "Bearer " + CONFIG["access_token"],
-               "Content-Type": "application/soap+xml"}
-    req = requests.Request("POST", url=url, params=params, headers=headers, data=body).prepare()
-    LOGGER.info("POST {}".format(req.url))
+def request_xsd():
+    url = 'https://adwords.google.com/api/adwords/reportdownload/v201702/reportDefinition.xsd'
+    req = requests.Request("GET", url=url).prepare()
+    LOGGER.info("GET {}".format(req.url))
     resp = SESSION.send(req)
 
-    if resp.status_code >= 400:
-        LOGGER.error("POST {} [{} - {}]".format(req.url, resp.status_code, resp.content))
-        sys.exit(1)
+    return resp.text
 
-    return resp.json()
- #TODO split on commas
-def sync_campaigns():
+# def refresh_access_token():
+#         refresh_token=CONFIG['refresh_token']
+#         refresh_req = request(url=REFRESH_TOKEN_URL,data={'grant_type': "refresh_token", 'refresh_token': refresh_token})
+#         refresh_resp = SESSION.send(refresh_req)
+#         refresh_json = refresh_resp.json()
+#         CONFIG['access_token'] = refresh_json['access_token']
+        
+
+#TODO split on commas
+def sync_campaigns(client):
     stream_name = 'campaigns'
-    request_body = BASE_REQUEST_XML.replace('{fields}', '<fields>TimeUnit</fields>') \
-                   .replace('{predicates}', '') \
-                   .replace('{ordering}', '') \
-                   .replace('{start-index}', '0') \
-                   .replace('{num-results}', '1') \
-                   .replace('{customer-id}', CONFIG['customer_ids']) \
-                   .replace('{developer-token}', CONFIG['developer_token'])
-                             
-    
-    
-    
-    request(BASE_URL + 'CampaignService', body=request_body)
-    
+    campaign_service = client.GetService('CampaignService', version=VERSION)
 
-def do_sync():
-    sync_campaigns()
+    offset = 0
+    selector = {
+        'fields': ['Id', 'Name'],
+        'ordering': {
+            'field': 'Name',
+            'sortOrder': 'ASCENDING'
+        },
+        'paging': {
+            'startIndex': str(offset),
+            'numberResults': str(PAGE_SIZE)
+        },
+        # 'predicates': {
+        #     'field': 'Labels',
+        #     'operator': 'CONTAINS_ANY',
+        #     'values': [label_id]
+        # }
+    }
+
+    more_pages = True
+    while more_pages:
+        page = campaign_service.get(selector)
+
+        # Display results.
+        if 'entries' in page:
+            for campaign in page['entries']:
+                print("{}".format(Client.dict(campaign)))
+
+                
+                singer.write_record(stream_name, Client.dict(campaign))
+                print ('Campaign found with Id \'%s\', name \'%s\', and labels: %s'
+                       % (campaign['id'], campaign['name'], campaign['labels']))
+            else:
+                print ('No campaigns were found.')
+                
+                offset += PAGE_SIZE
+                selector['paging']['startIndex'] = str(offset)
+                more_pages = offset < int(page['totalNumEntries'])
+                time.sleep(1)                                 
+
+def report_definition_service(client, report_type):
+    report_definition_service = client.GetService(
+        'ReportDefinitionService', version=VERSION)
+    fields = report_definition_service.getReportFields(report_type)
+    return fields
+
+REPORTING_REQUIRED_FIELDS = frozenset(["adGroupID", "campaignID", "account", "adID", "keywordID", "customerId", "date"])
+
+def inclusion_decision(field):
+    name = field['xmlAttributeName']
+    if name in REPORTING_REQUIRED_FIELDS:
+        return 'always'
+    return 'available'
+
+def do_sync(client):
+    top_res = request_xsd()
+    root = ET.fromstring(top_res)
+    path = list(root.find(".//*[@name='ReportDefinition.ReportType']/*"))
+    for p in path:
+        print(p.attrib['value'])
+    report_names = [p.attrib['value'] for p in path]
+    print(report_names)
+    #print("{}".format(path))
+    # fields = report_definition_service(client, "ADGROUP_PERFORMANCE_REPORT")
+    # schema = {}
+    # for field in fields:
+    #     schema[field['xmlAttributeName']] = {'description': field['displayFieldName'],
+    #                                          'type': field['fieldType'],
+    #                                          'behavior': field['fieldBehavior'],
+    #                                          'inclusion': inclusion_decision(field)}
+
+    # final_schema = {"type": "object",
+    #                 "properties": schema}
+    # print("{}".format(final_schema))
+        
+    #sync_campaigns(client)
     LOGGER.info("Sync complete")
 
 def main():
     config, state = utils.parse_args(REQUIRED_CONFIG_KEYS)
     CONFIG.update(config)
     STATE.update(state)
-    do_sync()
+
+    # Initialize the GoogleRefreshTokenClient using the credentials you received
+    # in the earlier steps.
+    oauth2_client = oauth2.GoogleRefreshTokenClient(
+        CONFIG['oauth_client_id'], \
+        CONFIG['oauth_client_secret'], \
+        CONFIG['refresh_token'])
+
+    # Initialize the AdWords client.
+    print("AdWordsClient args: {} | {} | {} | {}".format(CONFIG['developer_token'], oauth2_client, CONFIG['user_agent'], CONFIG['customer_ids']))
+    adwords_client = adwords.AdWordsClient(CONFIG['developer_token'], oauth2_client, user_agent=CONFIG['user_agent'], client_customer_id=CONFIG["customer_ids"])
+
+    do_sync(adwords_client)
 
 
 if __name__ == "__main__":
