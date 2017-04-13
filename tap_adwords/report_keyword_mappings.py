34c6766d57a94b54d651db1a3ac5c7f3db45ea83
Alexandra Smith <asmith@rjmetrics.com>
2017-04-13 14:20:45 -0400

    primary key mapping

---
+++ tap_adwords/report_keyword_mappings.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+REPORT_KEYWORD_MAPPINGS = {"ADGROUP_PERFROMANCE_REPORT": frozenset(['campaignID' 'adGroupID' 'customerID' 'account' 'day']),
+                           "AD_PERFORMANCE_REPORT": frozenset(['campaignID' 'adID' 'adGroupID' 'customerID' 'keywordID' 'account' 'day']),
+                           "CRITERIA_PERFORMANCE_REPORT": frozenset(['campaignID' 'adGroupID' 'customerID' 'keywordID' 'day']),
+                           "DISPLAY_KEYWORD_PERFORMANCE_REPORT": frozenset(['campaignID' 'adGroupID' 'customerID' 'keywordID' 'day']),
+                           "GEO_PERFORMANCE_REPORT": frozenset(['countryTerritory' 'campaignID' 'metroArea' 'mostSpecificLocation' 'adGroupID' 'city' 'customerID' 'region' 'account' 'day' 'adGroup']),
+                           "KEYWORDS_PERFORMANCE_REPORT": frozenset(['customerID', 'adGroupID', 'keywordID', 'account', 'adID'])}
