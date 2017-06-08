767479580c9194c58f55c6129e33a8c7535ff13f
Paul Santa Clara <psantaclara@rjmetrics.com>
2017-06-08 15:10:21 -0400

    filter out unselectable fields when fetching campaigns

--- tap_adwords/__init__.py
+++ tap_adwords/__init__.py
@@ -213,6 +213,18 @@ def suds_to_dict(obj):
             data[field] = suds_to_dict(val)
     return data
 
+campaigns_black_listed_fields = set(['networkSetting', 'conversionOptimizerEligibility', 'frequencyCap'])
+
+def filter_fields_to_sync_by_stream_name(stream_name, fields_to_sync):
+    if stream_name == 'campaigns':
+      return [f for f in fields_to_sync if f not in campaigns_black_listed_fields]
+    elif stream_name == 'ad_groups':
+      return fields_to_sync
+    elif stream_name == 'ads':
+      return fields_to_sync
+    else:
+      raise Exception("unrecognized generic stream_name {}".format(stream_name))
+
 def sync_generic_endpoint(stream_name, annotated_stream_schema, sdk_client):
     customer_id = sdk_client.client_customer_id
     discovered_schema = load_schema(stream_name)
@@ -220,11 +232,15 @@ def sync_generic_endpoint(stream_name, annotated_stream_schema, sdk_client):
     primary_keys = GENERIC_ENDPOINT_MAPPINGS[stream_name]['primary_keys']
     write_schema(stream_name, discovered_schema, primary_keys)
 
-    LOGGER.info("Syncing %s", stream_name)
     service_caller = sdk_client.GetService(service_name, version=VERSION)
-
+    LOGGER.info("Syncing %s", stream_name)
     field_list = get_fields_to_sync(discovered_schema, annotated_stream_schema)
+    LOGGER.info("Request fields: %s", field_list)
+    field_list = filter_fields_to_sync_by_stream_name(stream_name, field_list)
+    LOGGER.info("Filtered fields: %s", field_list)
+
     field_list = [f[0].upper()+f[1:] for f in field_list]
+
     offset = 0
     selector = {
         'fields': field_list,
