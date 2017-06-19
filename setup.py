b24ba444801c241a6ff976b36a91a208d1b8411f
Matthew Bilyeu <matt@stitchdata.com>
2017-06-19 11:22:35 -0400

    bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-adwords',
-      version="0.0.12",
+      version="0.0.13",
       description='Singer.io tap for extracting data from the Adwords api',
       author='Stitch',
       url='http://singer.io',
