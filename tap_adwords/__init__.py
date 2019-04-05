371634d38fabeb2bb7e2029acd1bdf2d79bba08a
Tim Visher <tim@stitchdata.com>
2019-04-05 14:52:39 +0000

    Differentiate between Stitch and other users for error messaging

--- tap_adwords/__init__.py
+++ tap_adwords/__init__.py
@@ -877,13 +877,21 @@ def check_selected_fields(stream, field_list, sdk_client):
                 field.xmlAttributeName, ",".join(field_errors)))
 
     if errors:
-        raise Exception((
-            "Fields selection violates Google's exclusion rules. "
-            "You may be able to correct this by going to the Stitch "
-            "UI and hovering over the question mark of an excluded "
-            "field if you're using this tap within Stitch."
-            "\n\t{}"
-        ).format("\n\t".join(errors)))
+        if sdk_client.user_agent == "Stitch Tap (+support@stitchdata.com)":
+            message_format = (
+                "Field selections violate Google's exclusion rules. "
+                "You should correct this by going to the Stitch "
+                "UI and hovering over the question mark of any excluded "
+                "field and following the instructions found in the "
+                "tooltip.\n\t{}"
+            )
+        else:
+            message_format = (
+                "Field selections violate Google's exclusion rules. "
+                "You should correct it in the catalog being passed "
+                "to the tap.\n\t{}"
+            )
+        raise Exception(message_format.format("\n\t".join(errors)))
 
 def do_discover_reports(sdk_client):
     url = 'https://adwords.google.com/api/adwords/reportdownload/{}/reportDefinition.xsd'.format(VERSION)
