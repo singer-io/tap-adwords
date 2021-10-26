7dc625ad77d4f9754547cb9215f38033d9fb31f7
Dan Mosora <30501696+dmosorast@users.noreply.github.com>
2021-10-26 19:10:25 +0000

    Version 1.13.1 and changelog

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-adwords',
-      version="1.13.0",
+      version="1.13.1",
       description='Singer.io tap for extracting data from the Adwords api',
       author='Stitch',
       url='http://singer.io',
